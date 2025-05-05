ce07c9e429e809c2c841da07632542ff863ccdcc
yowko <yowko0729@gmail.com>
2025-05-05 10:51:07 +0800

    replace http with https

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -28,7 +28,7 @@ export default {
       <>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🌱</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22https://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🌱</text></svg>"
         ></link>
         <title>{_title}</title>
         <meta name="description" content={description} />
